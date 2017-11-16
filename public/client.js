db118e76ef07a313614d98b3d62c6447470d3610
Glitch (rural-cloud) <none>
2017-11-16 18:57:54 +0000

    Update

--- public/client.js
+++ public/client.js
@@ -2,3 +2,11 @@
 				document.getElementById(x).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 
 			}
+
+      function red(x){
+        
+        window.location="https://rural-cloud.glitch.me"+x;
+      }
+if(document.readyState = "loading"){
+
+}
--- server.js
+++ server.js
@@ -4,9 +4,12 @@ var app = express();
 app.use(express.static('public'));
 
 app.get("/", (req,res)=>{
-	res.sendFile("/public/index.html")
+	res.sendFile(__dirname + "/public/index.html")
 })
 
+app.get("/contact", (req,res)=>{
+	res.sendFile(__dirname + "/public/contact.html")
+})
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
